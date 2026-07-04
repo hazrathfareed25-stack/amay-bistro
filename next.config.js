62ea2bf3c0249511c07e8fd41229b1249a7ee379
hazrathfareed25-stack <hazrathfareed25@gmail.com>
2026-07-04 12:48:39 +0530

    Configure Next.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com', 'images.pexels.com'],
+  },
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
